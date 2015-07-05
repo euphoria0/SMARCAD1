e7030155f4033b48e1feaa1b33d12e080c83fff5
dev-euphoria <github@euphoria.li>
2015-07-05 14:54:37 +0200

    project rename

--- injectScript.js
+++ injectScript.js
@@ -1,5 +1,5 @@
 var s = document.createElement('script');
-s.src = chrome.extension.getURL('xagent.js');
+s.src = chrome.extension.getURL('function.js');
 s.onload = function () {
     this.parentNode.removeChild(this);
 };
