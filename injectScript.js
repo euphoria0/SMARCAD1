2ae872ad8ad996d60a70799acebc49c501595d34
dev-euphoria <github@euphoria.li>
2015-07-06 16:18:24 +0200

    simple font detection

--- injectScript.js
+++ injectScript.js
@@ -5,3 +5,8 @@ script.onload = function () {
 };
 var parent = document.getElementsByTagName('head')[0] || document.documentElement;
 parent.appendChild(script);
+
+//var dummy = document.createElement('script');
+//dummy.src = chrome.extension.getURL('function.js');
+//var flash = document.getElementsByTagName('embed')[0];
+//flash.replaceChild(dummy,flash);
